76fbe68dde7d946ad091d14c3981ecf713e5aa2c
Dinesh020604 <92539956+Dinesh020604@users.noreply.github.com>
2024-03-05 20:24:34 +0530

    print_text.py

--- print_text.py
+++ print_text.py
@@ -1,2 +1,8 @@
-user_input=input()
-print(user_input)
+try:
+    width = int(input())
+    height = int(input())
+    def rectanglePerimeter(width, height):
+       return ((width + height)*2)
+    print(rectanglePerimeter(width, height))
+except EOFError as e:
+    print(end="")
